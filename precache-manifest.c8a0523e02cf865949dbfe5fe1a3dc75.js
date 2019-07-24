61eccd0b55512d2483a2a2ad5f079f701f287f4c
Dmitry Nozhenko <nozhenkod@gmail.com>
2019-07-24 16:08:28 +0300

    Updates

---
+++ precache-manifest.c8a0523e02cf865949dbfe5fe1a3dc75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5271935ae94f7f80ef2b79068697f1b",
+    "url": "/NozhenkoD/css-in-react/index.html"
+  },
+  {
+    "revision": "b5875c176ce523d6b69e",
+    "url": "/NozhenkoD/css-in-react/static/css/main.cb9bca7e.chunk.css"
+  },
+  {
+    "revision": "6f191b8f2f1bf3e63827",
+    "url": "/NozhenkoD/css-in-react/static/js/2.3b44a848.chunk.js"
+  },
+  {
+    "revision": "b5875c176ce523d6b69e",
+    "url": "/NozhenkoD/css-in-react/static/js/main.33f5e8d3.chunk.js"
+  },
+  {
+    "revision": "d3f139f2de252b195046",
+    "url": "/NozhenkoD/css-in-react/static/js/runtime~main.8aacd930.js"
+  },
+  {
+    "revision": "208b302db4897c25d97e3d98cf070b45",
+    "url": "/NozhenkoD/css-in-react/static/media/icons8-github.208b302d.svg"
+  },
+  {
+    "revision": "881300f7abf8d34d08af9340698b6621",
+    "url": "/NozhenkoD/css-in-react/static/media/icons8-medium-monogram.881300f7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fc215c8f69f4002ac5909904b7978e33.js"
+  "/NozhenkoD/css-in-react/precache-manifest.c8a0523e02cf865949dbfe5fe1a3dc75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
-
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NozhenkoD/css-in-react/index.html"), {
+  
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
